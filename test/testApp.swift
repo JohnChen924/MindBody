dada5d301cf31cb9bc10eb308507743f5a2a97bd
Noobies123456789 <hijohnchen@gmail.com>
2024-02-12 17:52:16 -0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by John Chen on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
